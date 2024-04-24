bc0b7631da52a6b8f6343695d0548c9b84d955c2
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-04-24 15:59:45 +0900

    데이터 등록

--- src/jobs/register.py
+++ src/jobs/register.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd 
 from typing import Union
+import os
+from src.middleware.logger import configure_logger
+
+logger = configure_logger(__name__)
 
 class DataRegister:
     def __init__(self):
@@ -8,4 +12,10 @@ class DataRegister:
 
     def register(self, predictions: Union[pd.DataFrame, np.ndarray], 
                  prediction_file_path: str):
-        pass
+        file, ext = os.path.splitext(prediction_file_path)
+        if ext != ".csv":
+            prediction_file_path = f"{file}.csv"
+            
+        logger.info(f"save prediction as csv: {prediction_file_path}")
+        predictions.to_csv(prediction_file_path, index=False)
+        return prediction_file_path
